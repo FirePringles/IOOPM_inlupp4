bb7851d34b8f9194bd980562f26b20995b16512e
Jonathan <gustafsson.jonathan@yahoo.se>
2019-11-26 17:17:05 +0100

    more ParserTest

--- TestParser.java
+++ TestParser.java
@@ -24,17 +24,31 @@ public class TestParser extends TestCase {
   public void testSubtraction() throws IOException{
     CalculatorParser parser = new CalculatorParser();
 
+    SymbolicExpression sub1 = new Subtraction(new Variable("x"), new Constant(10));
+    SymbolicExpression sub1Parsed = parser.parse("x-10\n");
+
+    assertTrue(sub1.equals(sub1Parsed));
+
   }
 
   @Test
   public void testMultiplication() throws IOException{
     CalculatorParser parser = new CalculatorParser();
+    SymbolicExpression mul1 = new Multiplication(new Multiplication(new Constant(10), new Constant(5)), new Variable("x"));
+    SymbolicExpression mul1Parsed = parser.parse("(((" + mul1.toString() + ")))\n");
+
+    assertTrue(mul1.equals(mul1Parsed));
 
   }
 
   @Test
   public void testDivision() throws IOException{
     CalculatorParser parser = new CalculatorParser();
+
+    SymbolicExpression div1 = new Division(new Variable("x"), new Constant(10));
+    SymbolicExpression div1Parsed = parser.parse(div1.toString() + "\n");
+
+    assertTrue(div1.equals(div1Parsed));
 
   }
 
@@ -64,11 +78,83 @@ public class TestParser extends TestCase {
 
   @Test
   public void testCos() throws IOException{
+    CalculatorParser parser = new CalculatorParser();
+
+    SymbolicExpression cos1 = new Cos(new Cos(new Cos(new Constant(10))));
+    SymbolicExpression cos1Parsed = parser.parse(cos1.toString() + "\n");
+
+    assertTrue(cos1.equals(cos1Parsed));
 
   }
 
   @Test
   public void testLog() throws IOException{
+    CalculatorParser parser = new CalculatorParser();
+
+    SymbolicExpression log1 = new Log(new Negation(new Negation(new Constant(10))));
+    SymbolicExpression log1Str = parser.parse(log1.toString() + "\n");
+
+    assertTrue(log1.equals(log1Str));
+
+  }
+
+  @Test
+  public void testExp() throws IOException{
+    CalculatorParser parser = new CalculatorParser();
+
+    SymbolicExpression exp1 = new Exp(new Constant(100));
+    SymbolicExpression exp1Parsed = parser.parse(exp1.toString() + "\n");
+
+    assertTrue(exp1.equals(exp1Parsed));
+
+  }
+
+
+  @Test
+  public void testQuit() throws IOException{
+    CalculatorParser parser = new CalculatorParser();
+
+    SymbolicExpression quit = Quit.instance();
+    SymbolicExpression quitParsed = parser.parse("quit\n");
+
+    assertTrue(quit.equals(quitParsed));
+    assertFalse(quit.equals(Clear.instance()));
+
+  }
+
+  @Test
+  public void testClear() throws IOException{
+    CalculatorParser parser = new CalculatorParser();
+
+    SymbolicExpression clear = Clear.instance();
+    SymbolicExpression clearParsed = parser.parse("clear\n");
+
+    assertTrue(clear.equals(clearParsed));
+  }
+
+  @Test
+  public void testVars() throws IOException{
+    CalculatorParser parser = new CalculatorParser();
+
+    SymbolicExpression vars = Vars.instance();
+    SymbolicExpression varsParsed = parser.parse("vars\n");
+
+    assertEquals(vars, varsParsed);
+
+  }
+
+  public void testNamedConstants() throws IOException{
+    CalculatorParser parser = new CalculatorParser();
+
+    SymbolicExpression pi = new Constant(Math.PI);
+    SymbolicExpression piParsed = parser.parse("pi\n");
+
+    SymbolicExpression e = new Constant(Math.E);
+    SymbolicExpression eParsed = parser.parse("e\n");
+
+
+     assertTrue(pi.equals(piParsed));
+     assertTrue(e.equals(eParsed));
 
   }
 
